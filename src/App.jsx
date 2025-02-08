406375b05ff6fa0d8599391dbc6c5e21bdb79f1f
unknown <najmulcse247@gmail.com>
2025-02-08 21:21:16 +0600

    react to do app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h2 className='text-5xl text-white p-3 font-bold  flex items-center justify-between min-h-screen uppercase bg-red-500 w-ful'>App</h2>
+  )
+}
+
+export default App
