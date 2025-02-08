7b3a82b711c1cf0057be88aefbac7c8dad170736
unknown <najmulcse247@gmail.com>
2025-02-08 22:31:07 +0600

    component divider done

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.id,
+    title: "title",
+    description: "description",
+    tags: ["web", "api"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -13,7 +22,7 @@ const TaskBoard = () => {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-            <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
