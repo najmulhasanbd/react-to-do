eefd6cf7cb3bf96d97c0ae9ddf7926802c88bc86
unknown <najmulcse247@gmail.com>
2025-02-09 00:13:29 +0600

    component divider done

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./addTaskModal";
 
 const TaskBoard = () => {
   const defaultTask = {
@@ -10,18 +11,24 @@ const TaskBoard = () => {
     description: "description",
     tags: ["web", "api"],
     priority: "High",
-    isFavorite: true,
+    isFavorite: false,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const[showAddModal, setShowAddModal]=useState(false);
+
+  function handleAddTask() {
+    alert("add task");
+  }
   return (
     <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
+          <TaskAction onAddClick={()=>setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
--- src/task/TaskList.jsx
+++ src/task/TaskList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegStar, FaStar } from "react-icons/fa6";
 
 const TaskList = ({ tasks }) => {
   return (
@@ -7,33 +8,33 @@ const TaskList = ({ tasks }) => {
         <thead>
           <tr>
             <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
-            <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">Title</th>
-            <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">Description</th>
-            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">Tags</th>
-            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">Priority</th>
-            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">Options</th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
+              Title
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">
+              Description
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
+              Tags
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+              Priority
+            </th>
+            <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+              Options
+            </th>
           </tr>
         </thead>
         <tbody>
           {tasks.map((task) => (
-            <tr key={task.id} className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
+            <tr
+              key={task.id}
+              className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
+            >
               <td>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-star"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="yellow"
-                  fill="yellow"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                </svg>
+                {task.isFavorite ? <FaStar color="yellow" /> :<FaRegStar color="green" /> }
               </td>
+
               <td>{task.title}</td>
               <td>{task.description}</td>
               <td>
